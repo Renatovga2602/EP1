6baf16c6e3e89a222deed8f5a308de12828dd649
Wneto2602 <renatovalerio25@gmail.com>
2016-09-21 22:49:00 -0300

    criação da classe Filtro Polarizado .hpp

---
+++ inc/FiltroPolarizado.hpp
@@ -0,0 +1,14 @@
+#ifndef FILTROPOLARIZADO_HPP
+#define FILTROPOLARIZADO_HPP
+  #include "Filtros.hpp"
+
+class FiltroPolarizado :
+public Filtros{
+public:
+    FiltroPolarizado();
+    ~FiltroPolarizado();
+        void aplicarFiltros(Imagem && imagem);
+
+
+};
+#endif
